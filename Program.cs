0b771dc95e6a2dbaa769ee554d701006fc5ce284
Albert Ngoudjou <angoudjou@yahoo.fr>
2018-02-04 07:35:53 -0500

    Checking of special character on a word

--- Program.cs
+++ Program.cs
@@ -8,32 +8,67 @@ namespace Lab6
 {
     class Program
     {
+        public static bool checkSpecialChar(string word)
+        {
+            char[] special_chars = { '@', '>', '<', '-', '\\','.','+','#','&','{','}','|', '=', ',' , '/', ':', '!', '§', '%', '*','$', '£', '(',')','[',']', '_'};
+            foreach (char c in special_chars)
+            {
+                if (word.Contains(c)) return true;
+            }
+            
+            return false;
+        }
         public static bool startWithVowel(string word)
         {
-            char a = word[0];
-            char[] vowels= {'a','e','i', 'o','u'};
-            foreach (char   item in vowels)
+            if (word.Length >0)
             {
-                if (item.Equals(a))
+                char a = word[0];
+                char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+                foreach (char item in vowels)
                 {
-                    return true;
+                    if (item.Equals(a))
+                    {
+                        return true;
+
+                    }
 
                 }
-                
             }
+            return false;
+        }
+        public static bool ContainsVowel(string word)
+        {
+            if (word.Length > 0)
+            {
+                char a = word[0];
+                char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+                foreach (char item in vowels)
+                {
+                    if (word.Contains (item))
+                    {
+                        return true;
 
+                    }
+
+                }
+            }
+            
             return false;
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Pig Latin translator");
-            Console.WriteLine("Please enter a word");
+            Console.Write("Please enter a word: ");
+            Console.WriteLine();
             string word = Console.ReadLine();
             string b = TranslateWordToPigLatin(word);
             Console.WriteLine(b);
             Console.WriteLine("Enter a sentence");
             string sentence = Console.ReadLine();
+            if(sentence.Length>0)
             Console.WriteLine(TranslateSentenceToPig(sentence));
+            else
+                Console.WriteLine("You did not enter a sentence");
             Console.ReadLine();
             
             ;
@@ -57,13 +92,24 @@ namespace Lab6
         {
             int i = 0, j;
             string begn = "";
-            //if contains special characters then we return opr if the character is not between a-Z
+            //if the contains character is not between a-Z
             foreach (char item in word.ToCharArray())
             {
                 if (int.TryParse(item.ToString(), out j)) return word;
 
             }
 
+            // checks if it contains specials keys
+            if (checkSpecialChar(word))
+            {
+                return word;
+            }
+
+            //if the word does contains vowels we return it as it is eg: By, my
+            if (!ContainsVowel(word))
+            {
+                return word;
+            }
           //  Console.WriteLine(startWithVowel(word.ToLower()));
             if (startWithVowel(word.ToLower()))
             {
@@ -71,7 +117,7 @@ namespace Lab6
             }
             else
             {
-                //move all consonnant that appearea before the firstc vowels
+                //move all consonnant that appearear before the first vowels
 
                 while (!startWithVowel(word.Substring(i)))
                 {
